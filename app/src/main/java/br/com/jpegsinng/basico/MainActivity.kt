cbec792912aa35aceb57e3882b54c4eff2516a95
Ingrid Silva <ingridsilvatec@outlook.com>
2025-01-29 01:10:49 -0300

    cria projeto

---
+++ app/src/main/java/br/com/jpegsinng/basico/MainActivity.kt
@@ -0,0 +1,32 @@
+package br.com.jpegsinng.basico
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.jpegsinng.basico.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val buttonToast = findViewById<Button>(R.id.buttonToast)
+        buttonToast.setOnClickListener {
+            val texto = editText.text.toString()
+            Toast.makeText(this, texto, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
